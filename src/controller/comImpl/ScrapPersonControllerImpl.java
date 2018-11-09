c87e29b936984a19c0a16430f1dec1826620d309
KOSTA <KOSTA@192.168.0.107>
2018-11-09 10:37:04 +0900

    여러가지.. ㅠㅠㅠㅠㅠㅠㅠ

---
+++ src/controller/comImpl/ScrapPersonControllerImpl.java
@@ -0,0 +1,50 @@
+package controller.comImpl;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+import controller.util.ModelAndView;
+import service.ApplicantService;
+import service.CompanyService;
+import service.aimpl.ApplicantServiceImpl;
+import service.aimpl.CompanyServiceImpl;
+import service.dto.ScrapPersonDTO;
+
+public class ScrapPersonControllerImpl implements Controller {
+	CompanyService asv = CompanyServiceImpl.getInstance();
+
+	@Override
+	public ModelAndView execute(HttpServletRequest req, HttpServletResponse rep) throws ServletException, IOException {
+
+		rep.setContentType("text/html;charset=UTF-8");
+	      req.setCharacterEncoding("UTF-8");
+	      
+	      String companyScraperId = req.getParameter("companyScraperId");
+	      String personTargetId = req.getParameter("personTargetId");
+
+	      ScrapPersonDTO spDTO = new ScrapPersonDTO(null, null, companyScraperId, personTargetId);      
+	      
+	      ModelAndView mv = new ModelAndView();
+	      String url = "";
+	      
+	      try {
+	         // 이동할 화면 설정
+	         url = "NewFile.html";
+	         asv.scrapPerson(spDTO);
+	         
+	      } catch (SQLException e) {
+	         e.printStackTrace();  //콘솔에 출력
+	         req.setAttribute("errorMsg", e.getMessage());
+	         url = "errors/error.jsp";
+	      }
+
+	      mv.setPath(url);
+	      return mv;
+	}
+
+}
